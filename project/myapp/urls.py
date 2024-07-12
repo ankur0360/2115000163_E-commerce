de1fb25ef1a6c0204b17aec322af37d11b61609c
Ankur Roy <ankurroyy0360roy@gmail.com>
2024-07-12 15:58:22 +0530

    Complete Project

--- project/myapp/urls.py
+++ project/myapp/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name = 'home'),
     path('catagory/products/register/', views.Products_view, name= 'register'),
-    path('catagory/', views.catagories, name='catagory'),
-
+    path('catagory/', views.catagories_items, name='catagory'),
+    path('catagory/products/<int:pk>/', views.catagories, name='catagories_items')
 ]
--- project/myapp/views.py
+++ project/myapp/views.py
@@ -15,7 +15,17 @@ def Products_view(request):
     context = {'form':form}
     return render(request, 'products.html', context)
 
-def catagories(request):
+def catagories_items(request):
     products_data = Products.objects.all()
     main_data = {'cata':products_data}
     return render(request,'catagories.html',{"cata":main_data})
+
+def catagories(request, pk = None):
+    if pk:
+        try:
+            product_item = Products.objects.get(pk=pk)
+        except Products.DoesNotExist:
+            product_item = ""
+    else:
+        product_item = ""
+    return render(request, 'product_item.html', {"product_item": product_item})
